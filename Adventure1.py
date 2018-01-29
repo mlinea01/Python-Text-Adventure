91a5e106dd04a87deb5d289b239b589af356fed1
Mike <33035299+mlinea01@users.noreply.github.com>
2018-01-29 04:10:03 -0500

    Add files via upload

---
+++ Adventure1.py
@@ -0,0 +1,8 @@
+def step1():
+    print("Now that you have completed your training, we can begin our first adventure!")
+    print("This is exciting!")
+    start = input("Are you ready to go? (yes or no)")
+    print("")
+
+    if start == "yes":
+        print("A long time ago, in a mythical land...")
