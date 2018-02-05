e5fae88a85ffaa81511021f85062d21f5986666c
Fredrick Esposito <fredrickesposito@gmail.com>
2018-02-05 10:11:25 -0500

    Tweaked the way the variable damage is printed in the Battle class
    In python, you can concatenate numbers to strings in 'print' calls using commas (i.e. "string1",int,"string2") which is a little more concise than "string1"+str(int)+"string2".

--- Battle.py
+++ Battle.py
@@ -5,7 +5,7 @@ class Battle:
         training_dummy = damage
         print("")
         print("It was a direct hit!")
-        print("You attacked the training dummy for " + str(damage) + " damage!")
+        print("You attacked the training dummy for",damage,"damage!")
         training_dummy -= damage
         print("training dummy:",training_dummy)
         print("")
