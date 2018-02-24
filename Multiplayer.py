be4d1a4a655c1c5972ee1b593249067e084fcf59
Fredrick Esposito <fredrickesposito@gmail.com>
2018-02-23 22:26:57 -0500

    Implemented a multiplayer game lobby
    Players enter the lobby when starting the game (by running TextAdventure.py). They can then start hosting a session for other players to connect to or connect to an existing session. The host can type "start game" to begin the game.

--- Multiplayer.py
+++ Multiplayer.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 import sys
+from copy import copy
+from Game import Game
 
 class Server:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,23 +11,39 @@ class Server:
     def __init__(self):
         HOST = socket.gethostbyname(socket.gethostname())
         PORT = 10000
+        self.playerOne = None
         self.sock.bind((HOST, PORT))
         self.sock.listen(1)
-        print("Server started on " + HOST + ", port: " + PORT)
-        print("Note: For other players to connect to your game session they must be on the same LAN as you"
+        self.game = None
+        print("Server started on " + HOST + ", port: " + str(PORT))
+        print("Note: For other players to connect to your game session they must be on the same LAN as you "
               "or you must set up port forwarding on your router. ")
         print("*Disclaimer* : please only invite players you trust to connect to your game session!")
+        print("")
+        print("Waiting for other players...")
+        print("Type 'start game' when all players have joined to start the game.")
 
     def connectionHandler(self, connection, address):
         while True:
             data = connection.recv(1024)
-            for con in self.connections:
-                if con != connection: con.send(data)
+
+            if connection == self.playerOne and self.game is None and str(data, 'utf-8') == "start game":
+                self.send_msg(bytes("Game Started!", 'utf-8'), self.connections)
+                break
+
             if not data:
                 self.connections.remove(connection)
                 connection.close()
                 print(str(address) + ": disconnected")
                 break
+
+            recipients = copy(self.connections)
+            recipients.remove(connection)
+            self.send_msg(data, recipients)
+
+    def send_msg(self, data, recipients):
+        for con in recipients:
+            con.send(data)
 
     def run(self):
         while True:
@@ -35,6 +53,8 @@ class Server:
             threadConnection.start()
             self.connections.append(connection)
             if len(self.connections)>1: print(str(address) + ": connected")
+            elif self.playerOne is None:
+                self.playerOne = connection
 
 class Client:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -42,25 +62,34 @@ class Client:
     def sendMsg(self):
         while True:
             self.sock.send(bytes(input(""), 'utf-8'))
+            if self.game is not None:
+                self.game.start()
+                break
 
-    def __init__(self, address):
+    def __init__(self, address, show_message=True):
         self.sock.connect(address)
+        self.chatMode = True
+        self.game = None
 
         threadInput = threading.Thread(target=self.sendMsg)
         threadInput.daemon = True
         threadInput.start()
-        print("Connected to " + str(address) )
+        if show_message: print("You connected to " + str(address) )
 
         while True:
             data = self.sock.recv(1024)
             if not data:
                 break
             print(str(data, 'utf-8'))
+            if str(data, 'utf-8') == "Game Started!":
+                self.game = threading.Thread(target=Game)
+                print("The game has been started, press Enter to begin.")
 
 
 class GameSession:
 
-    def __init__(self):
+    @classmethod
+    def start_session(cls):
         while True:
             try:
                 print("Please choose an option: ")
@@ -68,10 +97,10 @@ class GameSession:
                 print("\t 2. Connect to a session")
                 inputOption = int(input())
                 if inputOption == 1:
-                    threadServer = threading.Thread(target=self.createServer)
+                    threadServer = threading.Thread(target=cls.createServer)
                     threadServer.daemon = True
                     threadServer.start()
-                    Client( (socket.gethostbyname(socket.gethostname()), 10000) )
+                    Client( (socket.gethostbyname(socket.gethostname()), 10000), False )
                 else:
                     print("Enter the address to connect to: ")
                     addr = str(input())
@@ -81,6 +110,7 @@ class GameSession:
             except TimeoutError:
                 print("ERROR: Could not connect to game session. Please check your address entry and try again.")
 
-    def createServer(self):
+    @classmethod
+    def createServer(cls):
         server = Server()
         server.run()
