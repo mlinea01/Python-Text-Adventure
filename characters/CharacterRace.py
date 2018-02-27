633aaeb237cb63445c8c8d86dda3a4565f1f13b0
Mike <33035299+mlinea01@users.noreply.github.com>
2018-02-26 21:48:25 -0500

    added armor and mag resist paramter to the character race classes

--- characters/CharacterRace.py
+++ characters/CharacterRace.py
@@ -1,20 +1,24 @@
 from attacks import BasicAttacks
 from characters.CharacterInfo import Character
+from adventures.Armor import *
 
 
 class Gnome(Character):
     def __init__(self):
         desc = "The Gnome has increased speed and may be able to dodge attacks."
-        super().__init__("Gnome", desc, hp=10, mana=10, speed=20, attacks=[BasicAttacks.Punch(), BasicAttacks.Block()])
+        super().__init__("Gnome", desc, hp=10, mana=10, speed=20, attacks=[BasicAttacks.Punch(), BasicAttacks.Block()],
+                         armor=[leatherLegArmor(), leatherChestArmor(), leatherGloves(), leatherShoes()])
 
 
 class Ogre(Character):
     def __init__(self):
         desc = "The Ogre has increased HP and will last longer in fights."
-        super().__init__("Ogre", desc, hp=20, mana=10, speed=5, attacks=[BasicAttacks.Punch(), BasicAttacks.Block()])
+        super().__init__("Ogre", desc, hp=20, mana=10, speed=5, attacks=[BasicAttacks.Punch(), BasicAttacks.Block()],
+                         armor=[plateLegArmor(), plateChestArmor(), plateGloves(), plateShoes()])
 
 
 class Elf(Character):
     def __init__(self):
         desc = "The elf has increased endurance and will be able to fight longer in battle."
-        super().__init__("Elf", desc,  hp=10, mana=20, speed=5, attacks=[BasicAttacks.Punch(), BasicAttacks.Block()])
+        super().__init__("Elf", desc,  hp=10, mana=20, speed=5, attacks=[BasicAttacks.Punch(), BasicAttacks.Block()],
+                         armor=[clothLegArmor(), clothChestArmor(), clothGloves(), clothShoes()])
