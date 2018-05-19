6f0cccc78921edccfadf76fb1b7369f6bb2b11c8
Fredrick Esposito <fredrickesposito@gmail.com>
2018-05-18 23:07:25 -0400

    :(

--- Battle.py
+++ Battle.py
@@ -61,7 +61,9 @@ class Battle:
                     if chosen_attack is not None:
 
                         target_list = copy(fighters)
+                        time.sleep(0.2)
                         chosen_attack.filter_targets(fighter, target_list)
+                        time.sleep(0.2)
 
                         if chosen_attack.multi_target is False:
                             target_list = fighter.choose_target(target_list)
