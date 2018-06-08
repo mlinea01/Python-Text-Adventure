1b21eaec80b51f8b04f68a4a6a3160bc770b87ff
Mike <33035299+mlinea01@users.noreply.github.com>
2018-06-07 21:51:08 -0400

    resistances and weaknesses are only applied to attacks, not heals

--- characters/CharacterInfo.py
+++ characters/CharacterInfo.py
@@ -190,17 +190,18 @@ class Character:
                 else:
                     attack.damage -= (self.totalMagResist / 5)
 
-                for res in self.resistances:
-                    if attack.atkType == res:
-                        IO.print_text(self.name + " is resistant to " + attack.atkType.name + " damage!")
-                        attack.damage /= 2
-                        break
+                if attack.damage > 0:
+                    for res in self.resistances:
+                        if attack.atkType == res:
+                            IO.print_text(self.name + " is resistant to " + attack.atkType.name + " damage!")
+                            attack.damage /= 2
+                            break
 
-                for weak in self.weaknesses:
-                    if attack.atkType == weak:
-                        IO.print_text(self.name + " is weak against " + attack.atkType.name + " damage!")
-                        attack.damage *= 2
-                        break
+                    for weak in self.weaknesses:
+                        if attack.atkType == weak:
+                            IO.print_text(self.name + " is weak against " + attack.atkType.name + " damage!")
+                            attack.damage *= 2
+                            break
 
                 if attack.damage < 0:
                     attack.damage = 0
