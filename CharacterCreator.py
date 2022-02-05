ba531b1b7e75ea2e610e44d6978401649e7620d1
Fredrick Esposito <fredrickesposito@gmail.com>
2022-02-05 03:21:35 -0500

    Refactor Game class to CharacterCreator

---
+++ CharacterCreator.py
@@ -0,0 +1,105 @@
+from adventures.Forest.ForestAdventure import *
+from Battle import *
+from characters.Enemies import *
+from attacks.Spells import *
+from equipment.Weapons import *
+from characters.CharacterRace import *
+from characters.Player import Player
+
+class CharacterCreator:
+    @staticmethod
+    def make_character():
+        character_type = CharacterCreator.get_character_type()
+        character_race = CharacterCreator.get_character_race()
+        print("")
+        print("You chose " + character_race.name)
+        print(character_race.desc)
+        character_name = CharacterCreator.get_character_name()
+        player = Player(character_name, character_race, 0, character_type)
+        player.desc = input("Describe your character in one word: ").split(' ', 1)[0]
+        print("Hello " + player.name + " the " + player.desc + " " + character_type + " " + player.race)
+        weapon = CharacterCreator.get_character_weapon()
+        player.equip_weapon(weapon, False)
+        player.learn_attack(CharacterCreator.get_starting_spell(character_type))
+        return player
+
+    @staticmethod
+    def get_character_type():
+        characterTypes = ["Fire", "Water", "Earth", "Wind"]
+        character_type_selected = False
+        while not character_type_selected:
+            print("Choose a character type.\n")
+            typeNum = 1
+            for charType in characterTypes:
+                print(str(typeNum) + ". " + charType)
+                typeNum += 1
+
+            characterType = int(input("\nYour choice: "))
+            print("")
+
+            # confirm character type choice
+            print("You chose a " + characterTypes[characterType - 1] + " character.")
+            confirmed = int(input("Are you sure you want " + characterTypes[characterType - 1] + "? (1.yes, 2.no)")) == 1
+            if confirmed:
+                character_type_selected = True
+        return characterTypes[characterType - 1]
+
+    @staticmethod
+    def get_character_race():
+        # Prompt player to choose a Race for his/her character
+        characterRaces = [Gnome(), Ogre(), Elf(), Human()]
+        print("Choose a Race for your character.\n")
+        raceNum = 1
+        for raceType in characterRaces:
+            print(str(raceNum) + ". " + raceType.name)
+            raceNum += 1
+
+        characterRace = int(input("\nYour choice: "))
+        return characterRaces[characterRace - 1]
+
+
+    @staticmethod
+    def get_character_name():
+        # prompt the player for a character name
+        return input("\nCreate a name for your character: ")
+        
+    @staticmethod
+    def get_character_weapon():
+        # prompt the player to choose a starting weapon
+        weapons = [Sword(), WarHammer(), Staff(), BattleAxe(), Trident(), BowAndArrow()]
+        selected_weapon = None
+        while selected_weapon is None:
+            print("\nBefore you go out on your adventure, grab a weapon! (Choose One)\n")
+            weaponNum = 1
+            for weapon in weapons:
+                print(str(weaponNum) + ". " + weapon.name)
+                weaponNum += 1
+            selected_weapon = weapons[int(input("\nYour choice: ")) - 1]
+            print("")
+            print("The " + selected_weapon.name + " - " + selected_weapon.desc)
+            if int(input("Is this the weapon you want? (1.yes 2.no)")) != 1:
+                selected_weapon = None
+        return selected_weapon
+
+    @staticmethod
+    def get_starting_spell(character_type):
+        # prompt the player to choose a starting spell
+         # starting spells: fire, water, earth, wind
+        startingSpells = {
+            "Fire" : [fire_twister(), Scorch(), fire_breathe()],
+            "Water" : [whirl_pool(), Mist(), calming_waters()],
+            "Earth" : [Earthquake(), rock_slide(), rock_throw()],
+            "Wind" : [Tornado(), poison_breeze(), healing_breeze()]
+        }
+        chosen_spell = None
+        while chosen_spell is None:
+            print("\nYou will also need an ability to protect yourself.(Choose One)\n")
+            spellNum = 1
+            for spell in startingSpells[character_type]:
+                print(str(spellNum) + ". " + spell.name)
+                spellNum += 1
+            chosen_spell = startingSpells[character_type][int(input("\nYour choice: ")) - 1]
+            print(chosen_spell.name + " - " + chosen_spell.desc)
+            if int(input("Is this the spell you want? (1.yes 2.no)")) != 1:
+                chosen_spell = None
+        return chosen_spell
--- TextAdventure.py
+++ TextAdventure.py
@@ -1,6 +1,19 @@
-from Game import Game
+from time import sleep
+from Battle import Battle
+from CharacterCreator import CharacterCreator
+from adventures.Forest.ForestAdventure import ForestAdventure
+from characters.Enemies import TrainingDummy
 
 class Main:
 
-    Game.adventure_intro()
+    # Character creation
+    player = CharacterCreator.make_character()
+    sleep(2)
 
+    # Tutorial fight
+    print("")
+    print("\nYou'll need to learn how to fight out there. Let's see what ya got!")
+    print("Attack this training dummy to practice.\n")
+    print("")
+    sleep(5)
+    Battle().start(player, [TrainingDummy()])
