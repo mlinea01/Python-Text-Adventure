1288de518c743f6cf762bc6ca36f4d177c4097cb
Fredrick Esposito <fredrickesposito@gmail.com>
2022-02-05 13:50:24 -0500

    Refactor get_player_choice to be more concise

--- Utility.py
+++ Utility.py
@@ -5,17 +5,23 @@ def get_player_choice(choices, prompt_msg, confirm_func=None):
     choice = None
     while choice is None:
         print(prompt_msg)
-        for i in range(len(choices)):
-            print(str(i+1) + ". " + str(choices[i]))
+        print_numbered_list(choices)
         user_input = input("\nYour choice: ")
-        choice_index = int(user_input) - 1 if user_input.isnumeric() else None
-        if choice_index is None or choice_index < 0 or choice_index >= len(choices):
+        choice = choose(user_input, choices)
+        if choice is None:
             print("Invalid choice!\n")
             sleep(1)
-        else:
-            choice = choices[choice_index]
-            if confirm_func is not None:
-                confirmed = confirm_func(choice)
-                if not confirmed:
-                    choice = None
+        elif confirm_func is not None and not confirm_func(choice):
+            choice = None
     return choice
+
+def print_numbered_list(list):
+    for i in range(len(list)):
+        print(str(i+1) + ". " + str(list[i]))
+
+def choose(user_input, choices):
+    choice_index = int(user_input) - 1 if user_input.isnumeric() else None
+    if choice_index is None or choice_index < 0 or choice_index >= len(choices):
+        return None
+    else:
+        return choices[choice_index]
