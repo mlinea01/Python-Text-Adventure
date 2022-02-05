1606e7a85ee846dfbf7c82e61911fa7766ca38e5
Fredrick Esposito <fredrickesposito@gmail.com>
2022-02-05 13:48:34 -0500

    Handle invalid input in get_player_choice

--- Utility.py
+++ Utility.py
@@ -1,13 +1,21 @@
+from time import sleep
+
+
 def get_player_choice(choices, prompt_msg, confirm_func=None):
     choice = None
     while choice is None:
         print(prompt_msg)
         for i in range(len(choices)):
             print(str(i+1) + ". " + str(choices[i]))
-        choice_index = int(input("\nYour choice: ")) - 1
-        choice = choices[choice_index]
-        if confirm_func is not None:
-            confirmed = confirm_func(choice)
-            if not confirmed:
-                choice = None
+        user_input = input("\nYour choice: ")
+        choice_index = int(user_input) - 1 if user_input.isnumeric() else None
+        if choice_index is None or choice_index < 0 or choice_index >= len(choices):
+            print("Invalid choice!\n")
+            sleep(1)
+        else:
+            choice = choices[choice_index]
+            if confirm_func is not None:
+                confirmed = confirm_func(choice)
+                if not confirmed:
+                    choice = None
     return choice
